16e5b03a8eacada6723054f02be02e2921a58e61
Marian Echeverria <mariandreed@outlook.es>
2024-07-11 11:57:15 -0600

    creando repositorio2

---
+++ src/proyectodosgiblalalal/Proyectodosgiblalalal.java
@@ -0,0 +1,19 @@
+
+package proyectodosgiblalalal;
+
+
+public class Proyectodosgiblalalal {
+
+  
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {    
+                 System.out.println(i+"*10="+i*10);
+              
+            }
+          
+          
+        }
+        }
+    
+    
+
